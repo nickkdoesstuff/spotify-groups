485bbe96a472bb695276a659cc996e0a035258c2
nickkdoesstuff <69115738+nickkdoesstuff@users.noreply.github.com>
2024-04-02 18:24:48 +0100

    fixed date with no context

--- src/app/profile/[spotifyId]/page.tsx
+++ src/app/profile/[spotifyId]/page.tsx
@@ -11,6 +11,8 @@ import type { Metadata } from "next"
 import { generateMetadata as genMeta } from "@/lib/utils"
 import { ProfileCard } from "@/components/ProfileCard"
 import { MostPlayedArtists, MostPlayedArtistsLoading } from "@/components/MostPlayedArtists"
+import { DateWrapper } from "@/components/DateWrapper"
+import { DateControls } from "@/components/DateControls"
 
 export async function generateMetadata({ params }: { params: { spotifyId: string } }):Promise<Metadata> {
     const userList = await db.select().from(users).where(
@@ -36,20 +38,24 @@ export default async function Profile({ params }: { params: { spotifyId: string 
     const user = userList[0]
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
-            <ProfileCard user={user as User} isProfile />
-            <Suspense fallback={<RecentlyPlayedLoading />}>
-                <RecentlyPlayed user={user as User} isProfile />
-            </Suspense>
-            <Suspense fallback={<ListeningTimeLoading />}>
-                <ListeningTime user={user as User} isProfile />
-            </Suspense>
-            <Suspense fallback={<MostPlayedLoading />}>
-                <MostPlayed user={user as User} isProfile />
-            </Suspense>
-            <Suspense fallback={<MostPlayedArtistsLoading />}>
-                <MostPlayedArtists user={user as User} isProfile />
-            </Suspense>
-        </div>
+        <DateWrapper>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+                <ProfileCard user={user as User} isProfile />
+                <DateControls />
+                <Suspense fallback={<RecentlyPlayedLoading />}>
+                    <RecentlyPlayed user={user as User} isProfile />
+                </Suspense>
+                <Suspense fallback={<ListeningTimeLoading />}>
+                    <ListeningTime user={user as User} isProfile />
+                </Suspense>
+                <Suspense fallback={<MostPlayedLoading />}>
+                    <MostPlayed user={user as User} isProfile />
+                </Suspense>
+                <Suspense fallback={<MostPlayedArtistsLoading />}>
+                    <MostPlayedArtists user={user as User} isProfile />
+                </Suspense>
+            </div>
+        </DateWrapper>
+
     )
 }
