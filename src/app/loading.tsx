7cf27d96a82e92ee010071d2609daf04ebc1b52a
nickkdoesstuff <69115738+nickkdoesstuff@users.noreply.github.com>
2024-04-16 21:48:28 +0100

    no more loading bye bye

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from "lucide-react"
-
-export default function LoadingPage() {
-    return (
-        <div className="flex items-center justify-center h-[100vh] w-full">
-            <Loader2 className="h-4 w-4 animate-spin" />
-        </div>
-    )
-}
